8b2edf0c20adec6fb8d1bffe446627c3f68818d5
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2024-09-07 09:23:14 -0300

    ex 4

--- src/TP3/04/App.jsx
+++ src/TP3/04/App.jsx
@@ -1,5 +1,35 @@
+/*
+Modifique o componente Counter criado na Questão 2 para passar o valor do contador como uma prop chamada count para um novo componente funcional chamado DisplayCount. Este novo componente deve apenas exibir o valor recebido como prop.
+
+Instruções:
+
+- Crie o componente DisplayCount que aceita a prop count e exibe seu valor.
+- Atualize o componente Counter para passar o valor do contador para o componente DisplayCount como uma prop count.
+*/
+import { Component } from "react";
+
 export default function App() {
+  return <Counter />;
+}
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  render() {
     return (
-        <div>Hello World</div>
-    )
+      <div>
+        <DisplayCount count={this.state.count} />
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Incrementar +
+        </button>
+      </div>
+    );
+  }
+}
+
+function DisplayCount({ count }) {
+  return <p>{count}</p>;
 }
