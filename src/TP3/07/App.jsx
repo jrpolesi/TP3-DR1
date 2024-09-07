41e11cec5066b92a39592a26eb7053dc282b6aee
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2024-09-07 09:54:42 -0300

    ex 7

--- src/TP3/07/App.jsx
+++ src/TP3/07/App.jsx
@@ -1,5 +1,44 @@
+/*
+Modifique o componente TodoItem para adicionar um botão "Concluir" ao lado de cada tarefa. Quando este botão for clicado, a tarefa deve ser marcada como concluída, alterando visualmente sua aparência.
+
+Instruções:
+
+- Adicione a funcionalidade ao botão "Concluir" para atualizar o estado da tarefa no componente TodoItem, refletindo visualmente que a tarefa foi concluída.
+*/
+
+import { useState } from "react";
+
 export default function App() {
-    return (
-        <div>Hello World</div>
-    )
+  return <TodoList />;
+}
+
+const TASKS = [
+  { id: 1, task: "Estudar para a prova" },
+  { id: 2, task: "Ler um livro" },
+  { id: 3, task: "Fazer compras" },
+  { id: 4, task: "Assistir um filme" },
+  { id: 5, task: "Fazer exercícios" },
+];
+
+function TodoList() {
+  return (
+    <ul>
+      {TASKS.map(({ id, task }) => (
+        <TodoItem key={id} task={task} />
+      ))}
+    </ul>
+  );
+}
+
+function TodoItem({ task }) {
+  const [completed, setCompleted] = useState(false);
+
+  const textDecoration = completed ? "line-through" : "none";
+
+  return (
+    <li>
+      <span style={{ textDecoration }}>{task}</span>
+      <button onClick={() => setCompleted(true)}>Concluir</button>
+    </li>
+  );
 }
