aa3ae336e201c5f91511ea61953032fec0b414df
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2024-09-07 09:41:18 -0300

    ex 6

--- src/TP3/06/App.jsx
+++ src/TP3/06/App.jsx
@@ -1,5 +1,33 @@
+/* 
+Crie um componente chamado TodoList que exibe uma lista de tarefas. Cada tarefa deve ser representada por um componente funcional chamado TodoItem, que recebe uma prop chamada task contendo o texto da tarefa.
+
+Instruções:
+
+- Implemente os componentes TodoList e TodoItem, onde TodoList renderiza múltiplos TodoItems com diferentes tarefas passadas como props.
+*/
+
 export default function App() {
-    return (
-        <div>Hello World</div>
-    )
+  return <TodoList />;
+}
+
+const TASKS = [
+  { id: 1, task: "Estudar para a prova" },
+  { id: 2, task: "Ler um livro" },
+  { id: 3, task: "Fazer compras" },
+  { id: 4, task: "Assistir um filme" },
+  { id: 5, task: "Fazer exercícios" },
+];
+
+function TodoList() {
+  return (
+    <ul>
+      {TASKS.map(({ id, task }) => (
+        <TodoItem key={id} task={task} />
+      ))}
+    </ul>
+  );
+}
+
+function TodoItem({ task }) {
+  return <li>{task}</li>;
 }
