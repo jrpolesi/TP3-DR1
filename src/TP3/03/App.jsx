11371f3971bc08a428f14f9c4dcd7a93e67ab014
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2024-09-07 09:11:33 -0300

    ex 3

--- src/TP3/03/App.jsx
+++ src/TP3/03/App.jsx
@@ -1,5 +1,34 @@
+/* 
+Modifique o componente Greeting criado na Questão 1 para aceitar uma prop adicional chamada age. O componente deve exibir a saudação e a idade da seguinte forma: "Olá, {name}! Você tem {age} anos.".
+
+Instruções:
+
+- Atualize o componente Greeting para receber a prop age.
+- Exiba a saudação com o nome e a idade passados como props no formato "Olá, {name}! Você tem {age} anos.".
+*/
+
+const PEOPLE = [
+  { name: "João", age: 25 },
+  { name: "Maria", age: 30 },
+  { name: "José", age: 35 },
+  { name: "Ana", age: 40 },
+  { name: "Carlos", age: 45 },
+];
+
 export default function App() {
-    return (
-        <div>Hello World</div>
-    )
+  return (
+    <div>
+      {PEOPLE.map(({ name, age }) => (
+        <Greeting key={name} name={name} age={age} />
+      ))}
+    </div>
+  );
+}
+
+function Greeting({ name, age }) {
+  return (
+    <p>
+      Olá, {name}! Você tem {age} anos.
+    </p>
+  );
 }
